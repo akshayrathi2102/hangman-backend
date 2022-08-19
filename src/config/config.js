27badb7c55db1a5d57c2d3c4f79cb732f0095fd7
Akshay Rathi <akshayrathi2000@gmail.com>
2022-08-19 19:41:28 +0530

    Enable ssl for postgres

--- src/config/config.js
+++ src/config/config.js
@@ -21,5 +21,11 @@ module.exports = {
     database: DB_NAME,
     host: DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    }
   }
 };
